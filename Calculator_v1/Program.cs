92cbe01099fb4ff7291f561c1e67ec52eea70ba4
santhossundar <santhoshsunthar@gmail.com>
2023-04-01 12:02:07 +0530

    Add project files.

---
+++ Calculator_v1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("init");
+        }
+    }
+}
