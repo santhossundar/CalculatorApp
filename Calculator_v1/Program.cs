79822f5ac08205f851519f029122e8e1a11faaec
santhossundar <santhoshsunthar@gmail.com>
2023-04-01 13:01:31 +0530

    Added function to get user inputs

--- Calculator_v1/Program.cs
+++ Calculator_v1/Program.cs
@@ -6,7 +6,96 @@ namespace Calculator_v1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("init");
+            Console.WriteLine("------------ Menu ------------");
+            Console.WriteLine("(+) Addition");
+            Console.WriteLine("(-) Subtraction");
+            Console.WriteLine("(x) Multiplication");
+            Console.WriteLine("(/) divition");
+            Console.WriteLine("------------------------------");
+
+            Console.Write("Enter the operation: ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Addition();
+                    break;
+                case "-":
+                    Subtraction();
+                    break;
+                case "*":
+                    Multiplication();
+                    break;
+                case "/":
+                    Divition();
+                    break;
+            }
+        }
+
+        public static List<int> getUserInputs(string operation)
+        {
+            List<int> numbers = new List<int>();
+
+            if (operation == "+" || operation == "-" || operation == "x" || operation == "/")
+            {
+                int numberInputCount = 0;
+
+                while (true)
+                {
+                    Console.Write("Enter the number: ");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput != "exit")
+                    {
+                        bool isNumber = Int32.TryParse(userInput, out int number);
+
+                        if (isNumber)
+                        {
+                            numbers.Add(number);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    numberInputCount++;
+
+                    if ((operation == "-" || operation == "/") && numberInputCount > 2)
+                    {
+                        break;
+                    }
+                }
+
+                return numbers;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation!");
+
+                return null;
+            }   
+        }
+
+        public static void Divition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Multiplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Subtraction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Addition()
+        {
+            throw new NotImplementedException();
         }
     }
 }
