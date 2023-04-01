77b21b7bf2d6e5691c95aa42a9463d89f995a492
santhossundar <santhoshsunthar@gmail.com>
2023-04-01 20:32:19 +0530

    Added project files for calculator v2

---
+++ Calculator_v2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v2
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Calculator App v2");
+        }
+    }
+}
